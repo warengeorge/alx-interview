69348bf132aa3d1d931e5461cf3eaedf964be97a
warengeorge <georgeomiebi@gmail.com>
2023-04-26 21:14:35 +0100

    modified for loop

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -4,13 +4,26 @@ Task: Lockboxes
 """
 
 def canUnlockAll(boxes):
-    unlocked = [0]
+    """
+    Check if all boxes can be opened
+    """
+    # check if boxes is empty
+    if len(boxes) == 0:
+        return False
+
+    unlocked = set()
+    unlocked.add(0)
+
     for count, value in enumerate(boxes):
-        if not value:
-            continue
+        # check if index box can't be unlocked
+        if count not in unlocked:
+            return False
+
+        # add key indexes from unlocked box
         for key in value:
-            if key < len(boxes) and key not in unlocked and key != count:
-                unlocked.append(key)
-    if len(unlocked) == len(boxes):
-        return True
-    return False
+            # check each key index and update the set
+            if key < len(boxes) and key > count:
+                unlocked.update(boxes[key])
+        unlocked.update(value)
+
+    return True
