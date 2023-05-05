2581f59fb6478fee931346832a0487ceba9f7f1f
warengeorge <georgeomiebi@gmail.com>
2023-05-05 03:09:29 +0100

    Modified if statement

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -4,15 +4,16 @@ minimum operations
 ''' 
 
 def minOperations(n):
-    if n == 1:
+
+    if not isinstance(n, int):
         return 0
-    if n < 1:
-        return 0
+
     operations = 0
     i = 2
-    while i <= n:
-        while n % i == 0:
-            operations += 2
-            n //= i
+    while (i <= n):
+        if not (n % i):
+            n = int(n/i)
+            operations += i
+            i = 1
         i += 1
     return operations
