85be9a15050a9011cb2526b03945ac801f6f5574
warengeorge <georgeomiebi@gmail.com>
2023-05-19 03:03:07 +0100

    Fix: condition statement

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -6,21 +6,24 @@ set represents a valid UTF-8 encoding
 
 
 def validUTF8(data):
-
     bytes_remaining = 0
 
     for byte in data:
         if bytes_remaining == 0:
-            if byte >> 5 == 0b110 or byte >> 5 == 0b1110:
+            if byte >> 7 == 0b0:
+                bytes_remaining = 0
+            elif byte >> 5 == 0b110:
                 bytes_remaining = 1
-            elif byte >> 4 == 0b1110
+            elif byte >> 4 == 0b1110:
                 bytes_remaining = 2
             elif byte >> 3 == 0b11110:
                 bytes_remaining = 3
-            elif byte >> 7 == 0b1:
+            else:
                 return False
         else:
-            if byte >> 6 != 0b10:
+            if byte >> 6 == 0b10:
+                bytes_remaining -= 1
+            else:
                 return False
-            bytes_remaining -= 1
+
     return bytes_remaining == 0
